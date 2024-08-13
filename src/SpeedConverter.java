4f31f406734d6eae87ce042dd8c6f310424b9a49
Joseph-ui-sketch <hernajos000@warren.k12.in.us>
2024-08-13 13:55:49 -0400

    anyway

---
+++ src/SpeedConverter.java
@@ -0,0 +1,24 @@
+public class SpeedConverter {
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHours(1.5));
+        System.out.println(toMilesPerHours(10.25));
+        System.out.println(toMilesPerHours(-5.6));
+        System.out.println(toMilesPerHours(25.42));
+        System.out.println(toMilesPerHours(75.114));
+
+
+
+    }
+
+    public static long toMilesPerHours(double kilometersPerhour) {
+    if (kilometersPerhour < 0) {
+        return -1;
+    } else {
+        double mph = kilometersPerhour / 1.609;
+            return Math.round(mph);
+
+        }
+    }
+
+
+}
